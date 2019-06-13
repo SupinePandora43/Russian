c5cb5f5b3fad9e018e81a259a21fb207b6c62849
SupinePandora43 <xbox.account@bk.ru>
2019-06-13 22:48:32 +0500

    appetize_url.sh

--- travis_scripts/deploy.js
+++ travis_scripts/deploy.js
@@ -1,35 +1,33 @@
-console.log("Deploying...")
+console.log("DEPLOY: Deploying...")
+var fs = require("fs")
 var child_process = require("child_process")
-function deploy(){
+function deploy() {
 	child_process.exec(`bash -c \'curl https://${process.env.APPETIZE_TOKEN}@api.appetize.io/v1/apps` +
-	` -F "file=@./app/build/outputs/apk/debug/app-debug.apk"` +
-	` -F "platform=android"\'`,
-	(error, stdout, stderr) => {
-		if (error == null) {
-			var output = JSON.parse(stdout)
-			console.log(output.publicURL)
-			console.log("Deployed!")
-			if (process.env.CI == "true") {
-				console.log("CI")
-				if (process.platform == "linux"||process.platform == "darwin") {
-					child_process.exec("export APPETIZE_URL=" + output.publicURL, (error,stdout,stderr)=>{
-						console.log(error)
-						console.log(stdout)
-						console.log(stderr)
-					})
-					process.env.APPETIZE_URL = output.publicURL
-					console.log("(LINUX/OSX) APPETIZE_URL setted to " + output.publicURL)
-				} else if (process.platform == "win32") {
-					child_process.exec("set APPETIZE_URL=" + output.publicURL)
-					console.log("(WIN32) APPETIZE_URL setted to " + output.publicURL)
+		` -F "file=@./app/build/outputs/apk/debug/app-debug.apk"` +
+		` -F "platform=android"\'`,
+		(error, stdout, stderr) => {
+			if (error == null) {
+				var output = JSON.parse(stdout)
+				console.log(output.publicURL)
+				console.log("DEPLOY: Deployed!")
+				if (process.env.CI == "true") {
+					console.log("DEPLOY: CI")
+					if (process.platform == "linux" || process.platform == "darwin") {
+						fs.writeFile(__dirname + "/appetize_url.sh", "export APPETIZE_URL=" + output.publicURL, (err) => {
+							if (err == null) {
+								console.log("DEPLOY: Open appetize_url.sh");
+							} else {
+								console.log("DEPLOY: Error while saving appetize_url")
+							}
+						})
+					}
 				}
+			} else {
+				console.error("DEPLOY: ERROR:")
+				console.error(error)
+				console.error(stderr)
 			}
-		} else {
-			console.error("ERROR:")
-			console.error(error)
-			console.error(stderr)
-		}
-	})
+		})
 
 }
 deploy()
